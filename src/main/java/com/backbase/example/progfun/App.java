0507f31091f479cf46866422c1489b637da18468
Zoltan Altfatter <altfatterz@gmail.com>
2014-02-19 11:51:46 +0100

    initial commit

---
+++ src/main/java/com/backbase/example/progfun/App.java
@@ -0,0 +1,12 @@
+package com.backbase.example.progfun;
+
+import com.backbase.progfun.dependency.Hello;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println(new Hello().sayHello("Zoltan"));
+
+    }
+}
